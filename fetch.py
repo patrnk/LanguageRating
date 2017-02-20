883e6540af86f2dc07026c0708743e056b272ea1
Paul Vergeev <paulvergeev@gmail.com>
2017-02-20 18:26:58 +0300

    Implement get_argument_parser

--- fetch.py
+++ fetch.py
@@ -1,6 +1,7 @@
 from json import load
 from json import dump
 from os.path import exists
+from argparse import ArgumentParser
 
 
 def load_data_from_json_file(filepath):
@@ -15,8 +16,16 @@ def save_data_to_json_file(data, filepath):
         return dump(data, f)
 
 
+def get_argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument('number_of_vacancies', help='number of vacancies to retrieve')
+    parser.add_argument('credentials_file', 
+                        help='json file containing cliend_id and client_secret fields')
+    return parser
+
+
 if __name__ == '__main__':
-    args = get_argument_parser()
+    args = get_argument_parser().parse_args()
     credentials = load_data_from_json_file(args.credentials_file)
     vacancies = get_vacancy_list(args.number_of_vacancies, credentials)
     save_data_to_json_file(vacancies)
