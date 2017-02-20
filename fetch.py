619ac28506ca937c4c2b1154f6c1b4177cda8738
Paul Vergeev <paulvergeev@gmail.com>
2017-02-20 20:19:37 +0300

    Implement get_vacancy_list

--- fetch.py
+++ fetch.py
@@ -4,6 +4,23 @@ import argparse
 import requests
 
 
+#TODO: add count and page params
+def make_get_request_to_superjob(method, secret_key):
+    headers = { 'X-Api-App-Id': secret_key }
+    params = { 'keyword': 'программист' }   #FIXME: move to get_vacancy_list
+    kwargs = { 'headers': headers, 'params': params }
+    return requests.request(method='GET', 
+                            url='https://api.superjob.ru/2.0/%s/' % method, 
+                            **kwargs)
+
+
+def get_vacancy_list(number_of_vacancies, secret_key):
+    response = make_get_request_to_superjob('vacancies', key)
+    if not response.ok:
+        return None
+    return response.json()['objects']
+
+
 def get_argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('number_of_vacancies', help='number of vacancies to retrieve')
@@ -16,6 +33,6 @@ if __name__ == '__main__':
     args = get_argument_parser().parse_args()
     key = os.environ['SUPERJOB_SECRET_KEY']
     vacancies = get_vacancy_list(args.number_of_vacancies, key)
-    print(vacancies.text)
+    if vacancies is None:
+        exit('Sorry, couldn\'t get the list')
     json.dump(vacancies, args.outfile)
-    #save_data_to_json_file(vacancies, 'test.json')
