589c7705c2a92c45a75340d43d3030d446ef67fc
Paul Vergeev <paulvergeev@gmail.com>
2017-02-20 20:53:28 +0300

    Refactor make_get_request_to_superjob

--- fetch.py
+++ fetch.py
@@ -4,10 +4,8 @@ import argparse
 import requests
 
 
-#TODO: add count and page params
-def make_get_request_to_superjob(method, secret_key):
+def make_get_request_to_superjob(method, secret_key, params):
     headers = { 'X-Api-App-Id': secret_key }
-    params = { 'keyword': 'программист' }   #FIXME: move to get_vacancy_list
     kwargs = { 'headers': headers, 'params': params }
     return requests.request(method='GET', 
                             url='https://api.superjob.ru/2.0/%s/' % method, 
@@ -15,7 +13,10 @@ def make_get_request_to_superjob(method, secret_key):
 
 
 def get_vacancy_list(number_of_vacancies, secret_key):
-    response = make_get_request_to_superjob('vacancies', key)
+    #TODO: add additional keywords
+    #TODO: download exactly number_of_vacancies
+    params = { 'keyword': 'программист', 'count': 100 }
+    response = make_get_request_to_superjob('vacancies', key, params)
     if not response.ok:
         return None
     return response.json()['objects']
@@ -23,7 +24,8 @@ def get_vacancy_list(number_of_vacancies, secret_key):
 
 def get_argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('number_of_vacancies', help='number of vacancies to retrieve')
+    parser.add_argument('-t', '--top', type=int, default=100,
+                        help='number of vacancies to retrieve')
     parser.add_argument('-o', '--outfile', type=argparse.FileType('w'),
                         default='output.json', help='output JSON file')
     return parser
@@ -32,7 +34,7 @@ def get_argument_parser():
 if __name__ == '__main__':
     args = get_argument_parser().parse_args()
     key = os.environ['SUPERJOB_SECRET_KEY']
-    vacancies = get_vacancy_list(args.number_of_vacancies, key)
+    vacancies = get_vacancy_list(args.top, key)
     if vacancies is None:
         exit('Sorry, couldn\'t get the list')
     json.dump(vacancies, args.outfile)
