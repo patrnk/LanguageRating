7f6f8becb0b4ed37210841f94cbc2a6a01c42d1d
Paul Vergeev <paulvergeev@gmail.com>
2017-02-23 08:24:08 +0300

    Implement load_vacancy_list

--- rate.py
+++ rate.py
@@ -11,6 +11,10 @@ def print_stats_for_each_language(language_stats):
     pass
 
 
+def load_vacancy_list(inputfile):
+    return json.load(inputfile)
+
+
 def get_target_languages():
     pass
 
@@ -28,7 +32,7 @@ def get_argument_parser():
 
 if __name__ == '__main__':
     args = get_argument_parser().parse_args()
-    vacancies = json.load(args.infile)
+    vacancies = load_vacancy_list(args.infile)
     languages = get_target_languages()
     stats = get_stats_for_each_language(vacancies, languages)
     print_stats_for_each_language(stats)
